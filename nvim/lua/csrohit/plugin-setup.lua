ee580d9b526359f394a1f772ce77369c6f9781e9
Rohit <nehalnimkar@gmail.com>
2025-08-02 15:19:54 +0530

    Update lazy config

--- nvim/lua/csrohit/plugin-setup.lua
+++ nvim/lua/csrohit/plugin-setup.lua
@@ -1,16 +1,17 @@
--- Install package manager
---    https://github.com/folke/lazy.nvim
---    `:help lazy.nvim.txt` for more info
-local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system {
-    'git',
-    'clone',
-    '--filter=blob:none',
-    'https://github.com/folke/lazy.nvim.git',
-    '--branch=stable', -- latest stable release
-    lazypath,
-  }
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+    local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+    local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+    if vim.v.shell_error ~= 0 then
+        vim.api.nvim_echo({
+            { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+            { out,                            "WarningMsg" },
+            { "\nPress any key to exit..." },
+        }, true, {})
+        vim.fn.getchar()
+        os.exit(1)
+    end
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -19,8 +20,15 @@ vim.opt.rtp:prepend(lazypath)
 --
 --  You can also configure plugins after the setup call,
 --    as they will be available in your neovim runtime.
-require('lazy').setup({{import = 'csrohit.plugins'}, {import = "csrohit.plugins.lsp"}})
-
--- stup for nvim-
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
+require("lazy").setup({
+    spec = {
+        -- import your plugins
+        { import = "csrohit.plugins" },
+        { import = "csrohit.plugins.lsp" }
+    },
+    -- Configure any other settings here. See the documentation for more details.
+    -- colorscheme that will be used when installing plugins.
+    install = { colorscheme = { "kanagawa" } },
+    -- automatically check for plugin updates
+    checker = { enabled = true },
+})
