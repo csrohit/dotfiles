bf7f7ebdba04a00dc4f01c3684cef3c9cc84dd1e
Rohit Nimkar <nehalnimkar@gmail.com>
2025-12-30 16:21:22 +0530

    Fix breaking changes for tree-sitter

--- nvim/lua/csrohit/plugins/treesitter.lua
+++ nvim/lua/csrohit/plugins/treesitter.lua
@@ -1,112 +1,31 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    dependencies =
-    {
-        'nvim-treesitter/nvim-treesitter-textobjects',
-    },
-    version = false, -- last release is way too old and doesn't work on Windows
-    build = ":TSUpdate",
-    event = { "VeryLazy" },
-    lazy = vim.fn.argc(-1) == 0, -- load treesitter early when opening a file from the cmdline
-    cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" },
-    keys = {
-        { "<c-space>", desc = "Increment Selection" },
-        { "<bs>",      desc = "Decrement Selection", mode = "x" },
-    },
-    opts_extend = { "ensure_installed" },
-    opts = {
-        highlight = { enable = true },
+  "nvim-treesitter/nvim-treesitter",
+  branch = "main", -- CRITICAL: Use the new rewrite branch
+  build = ":TSUpdate",
+  event = { "VeryLazy" },
+  lazy = vim.fn.argc(-1) == 0,
+  config = function()
+    local ts = require("nvim-treesitter")
 
-        indent = { enable = true },
-        -- Install parsers synchronously (only applied to `ensure_installed`)
-        sync_install = false,
+    -- 1. Setup Parsers (Replaces ensure_installed)
+    ts.install({
+      "bash", "c", "diff", "html", "javascript", "json", "jsonc",
+      "lua", "luadoc", "luap", "markdown", "markdown_inline",
+      "printf", "python", "query", "regex", "toml", "vim",
+      "vimdoc", "xml", "yaml",
+    })
 
-        -- Automatically install missing parsers when entering buffer
-        -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-        auto_install = true,
+    -- 2. Enable Highlighting (Replaces highlight = { enable = true })
+    -- The new branch requires using Neovim's native Treesitter API
+    vim.api.nvim_create_autocmd("FileType", {
+      callback = function()
+        local buf = vim.api.nvim_get_current_buf()
+        pcall(vim.treesitter.start, buf)
+      end,
+    })
 
-        -- List of parsers to ignore installing (or "all")
-        ignore_install = {},
-        ensure_installed = {
-            "bash",
-            "c",
-            "diff",
-            "html",
-            "javascript",
-            "json",
-            "jsonc",
-            "lua",
-            "luadoc",
-            "luap",
-            "markdown",
-            "markdown_inline",
-            "printf",
-            "python",
-            "query",
-            "regex",
-            "toml",
-            "vim",
-            "vimdoc",
-            "xml",
-            "yaml",
-        },
-        incremental_selection = {
-            enable = true,
-            keymaps = {
-                init_selection = "<C-space>",
-                node_incremental = "<C-space>",
-                scope_incremental = false,
-                node_decremental = "<bs>",
-            },
-        },
-
-        -- 🧠 Text Objects
-        textobjects = {
-            select = {
-                enable = true,
-                lookahead = true,
-                keymaps = {
-                    ["af"] = "@function.outer",
-                    ["if"] = "@function.inner",
-                    ["ac"] = "@class.outer",
-                    ["ic"] = "@class.inner",
-                    ["ap"] = "@parameter.outer",
-                    ["ip"] = "@parameter.inner",
-                },
-            },
-            move = {
-                enable = true,
-                set_jumps = true,
-                goto_next_start = {
-                    ["]f"] = "@function.outer",
-                    ["]c"] = "@class.outer",
-                },
-                goto_next_end = {
-                    ["]F"] = "@function.outer",
-                    ["]C"] = "@class.outer",
-                },
-                goto_previous_start = {
-                    ["[f"] = "@function.outer",
-                    ["[c"] = "@class.outer",
-                },
-                goto_previous_end = {
-                    ["[F"] = "@function.outer",
-                    ["[C"] = "@class.outer",
-                },
-            },
-            swap = {
-                enable = true,
-                swap_next = {
-                    ["<leader>a"] = "@parameter.inner",
-                },
-                swap_previous = {
-                    ["<leader>A"] = "@parameter.inner",
-                },
-            },
-        },
-
-    },
-    config = function(_, opts)
-        require("nvim-treesitter.configs").setup(opts)
-    end,
+    -- 3. Setup Indentation (Replaces indent = { enable = true })
+    -- Note: Indentation is still evolving on the main branch
+    vim.opt.smartindent = false -- Often recommended to let TS handle it
+  end,
 }
