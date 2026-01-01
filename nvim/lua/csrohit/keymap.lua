493a18b03cc09de9fa2e121eeee3b03dd1986cd8
Rohit Nimkar <nehalnimkar@gmail.com>
2026-01-01 12:20:52 +0530

    add shortcut to move lines

--- nvim/lua/csrohit/keymap.lua
+++ nvim/lua/csrohit/keymap.lua
@@ -18,3 +18,11 @@ function Vmap(lhs, rhs, opts)
 end
 Nmap("<TAB>", ":bn<CR>", { desc = "Next buffer" })
 Nmap("<S-TAB>", ":bp<CR>", { desc = "Previous buffer" })
+
+-- Move Lines
+vim.keymap.set("n", "<M-Down>", "<cmd>m .+1<cr>==", { desc = "Move down" })
+vim.keymap.set("n", "<M-Up>", "<cmd>m .-2<cr>==", { desc = "Move up" })
+vim.keymap.set("i", "<M-Down>", "<esc><cmd>m .+1<cr>==gi", { desc = "Move down" })
+vim.keymap.set("i", "<M-Up>", "<esc><cmd>m .-2<cr>==gi", { desc = "Move up" })
+vim.keymap.set("v", "<M-Down>", ":m '>+1<cr>gv=gv", { desc = "Move down" })
+vim.keymap.set("v", "<M-Up>", ":m '<-2<cr>gv=gv", { desc = "Move up" })
