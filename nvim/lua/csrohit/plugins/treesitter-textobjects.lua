a8cafacd8ccce8dbfd407eef3f4997fcd51d0178
Rohit Nimkar <nehalnimkar@gmail.com>
2026-01-01 21:40:21 +0530

    Add keymaps for textobjects

--- nvim/lua/csrohit/plugins/treesitter-textobjects.lua
+++ nvim/lua/csrohit/plugins/treesitter-textobjects.lua
@@ -1,14 +1,13 @@
 return {
     "nvim-treesitter/nvim-treesitter-textobjects",
-    branch = "main", -- CRITICAL: You must use the main branch
+    branch = "main",
     dependencies = { "nvim-treesitter/nvim-treesitter" },
     config = function()
         require("nvim-treesitter-textobjects").setup({
             select = {
                 enable = true,
-                lookahead = true, -- Automatically jump forward to textobj
+                lookahead = true,
                 keymaps = {
-                    -- Your exact old keymaps
                     ["af"] = "@function.outer",
                     ["if"] = "@function.inner",
                     ["ac"] = "@class.outer",
@@ -17,35 +16,37 @@ return {
                     ["ip"] = "@parameter.inner",
                 },
             },
-            move = {
-                enable = true,
-                set_jumps = true, -- Set jumps in the jumplist
-                goto_next_start = {
-                    ["]f"] = "@function.outer",
-                    ["]c"] = "@class.outer",
-                },
-                goto_next_end = {
-                    ["]F"] = "@function.outer",
-                    ["]C"] = "@class.outer",
-                },
-                goto_previous_start = {
-                    ["[f"] = "@function.outer",
-                    ["[c"] = "@class.outer",
-                },
-                goto_previous_end = {
-                    ["[F"] = "@function.outer",
-                    ["[C"] = "@class.outer",
-                },
-            },
             swap = {
                 enable = true,
                 swap_next = {
                     ["<leader>a"] = "@parameter.inner",
-                    ["<leader>s"] = "@assignment.outer", -- Add this to swap 'left = right' to 'right = left'
                 },
                 swap_previous = {
-                    ["<leader>A"] = "@parameter.inner",
-                    ["<leader>S"] = "@assignment.outer", -- Swap back to the left
+                    ["<leader>A"] = "@parameter.outer", -- Updated to match your manual keymap
+                },
+            },
+            move = {
+                enable = true,
+                set_jumps = true, -- Check this: allows you to use Ctrl-o to jump back
+                goto_next_start = {
+                    ["]m"] = "@function.outer",
+                    -- You can pass a list of queries to cycle through
+                    ["]o"] = { "@loop.inner", "@loop.outer" },
+
+                    -- To use "locals" or "folds", we use the table syntax
+                    ["]s"] = { query = "@local.scope", query_group = "locals", desc = "Next scope" },
+                    ["]z"] = { query = "@fold", query_group = "folds", desc = "Next fold" },
+                },
+                goto_next_end = {
+                    ["]M"] = "@function.outer",
+                    ["]["] = "@class.outer",
+                },
+                goto_previous_start = {
+                    ["[m"] = "@function.outer",
+                },
+                goto_previous_end = {
+                    ["[M"] = "@function.outer",
+                    ["[]"] = "@class.outer",
                 },
             },
         })
